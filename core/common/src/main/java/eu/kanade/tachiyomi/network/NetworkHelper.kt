464227e47dd8fdb04b95b0d5300b58e561847446
Biplab8 <64832887+Biplab8@users.noreply.github.com>
2026-07-22 04:33:51 +0530

    Revert: Remove IgnoreGzipInterceptor and BrotliInterceptor - restore to stable state
    This reverts all changes related to adding IgnoreGzipInterceptor and BrotliInterceptor
    to the default client. These interceptors were causing crashes when extensions made
    network requests. The stable configuration without these interceptors allows all
    extensions (old and new) to work without crashes.

--- core/common/src/main/java/eu/kanade/tachiyomi/network/NetworkHelper.kt
+++ core/common/src/main/java/eu/kanade/tachiyomi/network/NetworkHelper.kt
@@ -3,14 +3,12 @@ package eu.kanade.tachiyomi.network
 import android.content.Context
 import eu.kanade.tachiyomi.network.interceptor.CloudflareInterceptor
 import eu.kanade.tachiyomi.network.interceptor.FlareSolverrInterceptor
-import eu.kanade.tachiyomi.network.interceptor.IgnoreGzipInterceptor
 import eu.kanade.tachiyomi.network.interceptor.UncaughtExceptionInterceptor
 import eu.kanade.tachiyomi.network.interceptor.UserAgentInterceptor
 import kotlinx.coroutines.CoroutineScope
 import okhttp3.Cache
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
-import okhttp3.brotli.BrotliInterceptor
 import okhttp3.logging.HttpLoggingInterceptor
 import java.io.File
 import java.util.concurrent.TimeUnit
@@ -37,8 +35,6 @@ class NetworkHelper(
             )
             .addInterceptor(UncaughtExceptionInterceptor())
             .addInterceptor(UserAgentInterceptor(::defaultUserAgentProvider))
-            .addInterceptor(IgnoreGzipInterceptor())
-            .addInterceptor(BrotliInterceptor)
             // TLMR -->
             .addInterceptor(FlareSolverrInterceptor(preferences))
         // <-- TLMR
